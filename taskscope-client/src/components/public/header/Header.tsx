b67bf85fbe569319ceb2310ef98cba0016acb07b
denys-pavskyi <den.pavski@gmail.com>
2025-10-22 02:46:14 +0300

    feat: Added Header component

---
+++ taskscope-client/src/components/public/header/Header.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import '/Header.scss'
+import { Layout, Typography} from 'antd'
+
+const { Header: AntHeader} = Layout;
+const { Title } = Typography;
+
+const Header: React.FC = () => {
+    return (
+        <AntHeader className="app-header">
+            <Title level={3} className="logo">TaskScope</Title>
+        </AntHeader>
+    );
+};
+
+export default Header;
