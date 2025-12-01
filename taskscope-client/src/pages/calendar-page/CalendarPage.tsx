b27d9eff7045a7787260c6507ad7f1750a0a9d00
denys-pavskyi <den.pavski@gmail.com>
2025-12-01 19:04:01 +0200

    refactor: removed unused pages

--- taskscope-client/src/pages/calendar-page/CalendarPage.tsx
+++
@@ -1,2 +0,0 @@
-import React from 'react';
-export const CalendarPage: React.FC = () => <h2>Calendar</h2>;
--- taskscope-client/src/routes/AppRouter.tsx
+++ taskscope-client/src/routes/AppRouter.tsx
@@ -1,6 +1,5 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import MainLayout from '../layouts/main/MainLayout';
-import { CalendarPage } from '../pages/calendar-page/CalendarPage';
 import { TagsPage } from '../pages/tags-page/TagsPage';
 import TodayPage from '../pages/today-page/TodayPage';
 import { UpcomingPage } from '../pages/upcoming-page/UpcomingPage';
@@ -13,7 +12,6 @@ export const AppRouter = () => (
         <Route path="/today" element={<TodayPage />} />
         <Route path="/upcoming" element={<UpcomingPage />} />
         <Route path="/tags" element={<TagsPage />} />
-        <Route path="/calendar" element={<CalendarPage />} />
       </Route>
     </Routes>
   </BrowserRouter>
