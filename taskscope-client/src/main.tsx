30114d563f971db8f6d09379bf65db8c8cff9742
denys-pavskyi <den.pavski@gmail.com>
2025-12-01 17:57:43 +0200

    feat: Added some pngs for future purposes, connected redux store

--- taskscope-client/src/main.tsx
+++ taskscope-client/src/main.tsx
@@ -2,9 +2,13 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import { AppRouter } from './routes/AppRouter.tsx'
 import React from 'react'
+import { Provider } from 'react-redux'
+import { store } from './store/store'
 
 createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <AppRouter />
+    <Provider store={store}>
+      <AppRouter />
+    </Provider>
   </React.StrictMode>
 )
